fd54a6dcbd3bc70746a932a814ad69904336c817
Henny-Willy <5954598+HennyWilly@users.noreply.github.com>
2025-09-14 15:20:47 +0200

    Add shadow resistance trigger and action to Lucifron

--- src/strategy/raids/moltencore/RaidMcActionContext.h
+++ src/strategy/raids/moltencore/RaidMcActionContext.h
@@ -10,6 +10,7 @@ class RaidMcActionContext : public NamedObjectContext<Action>
 public:
     RaidMcActionContext()
     {
+        creators["mc lucifron shadow resistance action"] = &RaidMcActionContext::lucifron_shadow_resistance_action;
         creators["mc magmadar fire resistance action"] = &RaidMcActionContext::magmadar_fire_resistance_action;
         creators["mc baron geddon fire resistance action"] = &RaidMcActionContext::baron_geddon_fire_resistance_action;
         creators["mc check should move from group"] = &RaidMcActionContext::check_should_move_from_group;
@@ -17,6 +18,7 @@ public:
     }
 
 private:
+    static Action* lucifron_shadow_resistance_action(PlayerbotAI* ai) { return new BossShadowResistanceAction(ai, "lucifron"); }
     static Action* magmadar_fire_resistance_action(PlayerbotAI* ai) { return new BossFireResistanceAction(ai, "magmadar"); }
     static Action* baron_geddon_fire_resistance_action(PlayerbotAI* ai) { return new BossFireResistanceAction(ai, "baron geddon"); }
     static Action* check_should_move_from_group(PlayerbotAI* ai) { return new McCheckShouldMoveFromGroupAction(ai); }
