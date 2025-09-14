a3f29fbc1f4d6340e02a106968a5c15e79f72c10
Henny-Willy <5954598+HennyWilly@users.noreply.github.com>
2025-09-14 12:11:19 +0200

    Refactor existing MC strategy

--- src/strategy/raids/moltencore/RaidMcTriggers.cpp
+++ src/strategy/raids/moltencore/RaidMcTriggers.cpp
@@ -4,12 +4,12 @@
 
 bool McLivingBombDebuffTrigger::IsActive()
 {
-    // if bot has barron geddon's living bomb, we need to add strat, otherwise we need to remove
+    // if bot has baron geddon's living bomb, we need to add strat, otherwise we need to remove
     // only do when fighting baron geddon (to avoid modifying strat set by player outside this fight)
     if (Unit* boss = AI_VALUE2(Unit*, "find target", "baron geddon"))
     {
         if (boss->IsInCombat())
-            return bot->HasAura(20475) != botAI->HasStrategy("move from group", BotState::BOT_STATE_COMBAT);
+            return bot->HasAura(SPELL_BARON_GEDDON_LIVING_BOMB) != botAI->HasStrategy("move from group", BOT_STATE_COMBAT);
     }
     return false;
 }
@@ -17,6 +17,6 @@ bool McLivingBombDebuffTrigger::IsActive()
 bool McBaronGeddonInfernoTrigger::IsActive()
 {
     if (Unit* boss = AI_VALUE2(Unit*, "find target", "baron geddon"))
-        return boss->HasAura(19695);
+        return boss->HasAura(SPELL_BARON_GEDDON_INFERNO);
     return false;
 }
