9e996b08a7db123476b28e0c22496595c9bd7d4d
Henny-Willy <5954598+HennyWilly@users.noreply.github.com>
2025-09-14 12:56:17 +0200

    Add fire resistance trigger and action to Baron Geddon

--- src/strategy/raids/moltencore/RaidMcActionContext.h
+++ src/strategy/raids/moltencore/RaidMcActionContext.h
@@ -10,11 +10,13 @@ class RaidMcActionContext : public NamedObjectContext<Action>
 public:
     RaidMcActionContext()
     {
+        creators["mc baron geddon fire resistance action"] = &RaidMcActionContext::baron_geddon_fire_resistance_action;
         creators["mc check should move from group"] = &RaidMcActionContext::check_should_move_from_group;
         creators["mc move from baron geddon"] = &RaidMcActionContext::move_from_baron_geddon;
     }
 
 private:
+    static Action* baron_geddon_fire_resistance_action(PlayerbotAI* ai) { return new BossFireResistanceAction(ai, "baron geddon"); }
     static Action* check_should_move_from_group(PlayerbotAI* ai) { return new McCheckShouldMoveFromGroupAction(ai); }
     static Action* move_from_baron_geddon(PlayerbotAI* ai) { return new McMoveFromBaronGeddonAction(ai); }
 };
--- src/strategy/raids/moltencore/RaidMcTriggerContext.h
+++ src/strategy/raids/moltencore/RaidMcTriggerContext.h
@@ -10,11 +10,13 @@ class RaidMcTriggerContext : public NamedObjectContext<Trigger>
 public:
     RaidMcTriggerContext()
     {
+        creators["mc baron geddon fire resistance trigger"] = &RaidMcTriggerContext::baron_geddon_fire_resistance_trigger;
         creators["mc living bomb debuff"] = &RaidMcTriggerContext::living_bomb_debuff;
         creators["mc baron geddon inferno"] = &RaidMcTriggerContext::baron_geddon_inferno;
     }
 
 private:
+    static Trigger* baron_geddon_fire_resistance_trigger(PlayerbotAI* ai) { return new BossFireResistanceTrigger(ai, "baron geddon"); }
     static Trigger* living_bomb_debuff(PlayerbotAI* ai) { return new McLivingBombDebuffTrigger(ai); }
     static Trigger* baron_geddon_inferno(PlayerbotAI* ai) { return new McBaronGeddonInfernoTrigger(ai); }
 };
