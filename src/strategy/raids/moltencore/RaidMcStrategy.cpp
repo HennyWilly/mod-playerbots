ac61f32cd354f30947845fe44d42acc836318dc7
Henny-Willy <5954598+HennyWilly@users.noreply.github.com>
2025-09-14 14:16:41 +0200

    Add fire resistance trigger and action to Magmadar

--- src/strategy/raids/moltencore/RaidMcStrategy.cpp
+++ src/strategy/raids/moltencore/RaidMcStrategy.cpp
@@ -4,6 +4,13 @@
 
 void RaidMcStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
 {
+    // Magmadar
+    // TODO: Fear ward / tremor totem, or general anti-fear strat development. Same as King Dred (Drak'Tharon) and faction commander (Nexus).
+    triggers.push_back(
+        new TriggerNode("mc magmadar fire resistance trigger",
+                        NextAction::array(0, new NextAction("mc magmadar fire resistance action", ACTION_RAID), nullptr)));
+
+    // Baron Geddon
     triggers.push_back(
         new TriggerNode("mc baron geddon fire resistance trigger",
                         NextAction::array(0, new NextAction("mc baron geddon fire resistance action", ACTION_RAID), nullptr)));
