be868122cb77ca2db0cdc1656487421e91661136
Henny-Willy <5954598+HennyWilly@users.noreply.github.com>
2025-09-14 14:16:41 +0200

    Add fire resistance trigger and action to Magmadar

--- src/strategy/raids/moltencore/RaidMcTriggerContext.h
+++ src/strategy/raids/moltencore/RaidMcTriggerContext.h
@@ -10,12 +10,14 @@ class RaidMcTriggerContext : public NamedObjectContext<Trigger>
 public:
     RaidMcTriggerContext()
     {
+        creators["mc magmadar fire resistance trigger"] = &RaidMcTriggerContext::magmadar_fire_resistance_trigger;
         creators["mc baron geddon fire resistance trigger"] = &RaidMcTriggerContext::baron_geddon_fire_resistance_trigger;
         creators["mc living bomb debuff"] = &RaidMcTriggerContext::living_bomb_debuff;
         creators["mc baron geddon inferno"] = &RaidMcTriggerContext::baron_geddon_inferno;
     }
 
 private:
+    static Trigger* magmadar_fire_resistance_trigger(PlayerbotAI* ai) { return new BossFireResistanceTrigger(ai, "magmadar"); }
     static Trigger* baron_geddon_fire_resistance_trigger(PlayerbotAI* ai) { return new BossFireResistanceTrigger(ai, "baron geddon"); }
     static Trigger* living_bomb_debuff(PlayerbotAI* ai) { return new McLivingBombDebuffTrigger(ai); }
     static Trigger* baron_geddon_inferno(PlayerbotAI* ai) { return new McBaronGeddonInfernoTrigger(ai); }
