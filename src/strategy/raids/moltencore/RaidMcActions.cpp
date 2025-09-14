2647019fcd90ad0b9ca9e87b5c7e8d9f56a826ba
Henny-Willy <5954598+HennyWilly@users.noreply.github.com>
2025-09-14 12:11:19 +0200

    Refactor existing MC strategy

--- src/strategy/raids/moltencore/RaidMcActions.cpp
+++ src/strategy/raids/moltencore/RaidMcActions.cpp
@@ -1,12 +1,13 @@
 #include "RaidMcActions.h"
 
 #include "Playerbots.h"
+#include "RaidMcTriggers.h"
 
 bool McCheckShouldMoveFromGroupAction::Execute(Event event)
 {
-    if (bot->HasAura(20475))  // barron geddon's living bomb
+    if (bot->HasAura(SPELL_BARON_GEDDON_LIVING_BOMB)) // baron geddon's living bomb
     {
-        if (!botAI->HasStrategy("move from group", BotState::BOT_STATE_COMBAT))
+        if (!botAI->HasStrategy("move from group", BOT_STATE_COMBAT))
         {
             // add/remove from both for now as it will make it more obvious to
             // player if this strat remains on after fight somehow
@@ -15,7 +16,7 @@ bool McCheckShouldMoveFromGroupAction::Execute(Event event)
             return true;
         }
     }
-    else if (botAI->HasStrategy("move from group", BotState::BOT_STATE_COMBAT))
+    else if (botAI->HasStrategy("move from group", BOT_STATE_COMBAT))
     {
         // add/remove from both for now as it will make it more obvious to
         // player if this strat remains on after fight somehow
@@ -28,15 +29,15 @@ bool McCheckShouldMoveFromGroupAction::Execute(Event event)
 
 bool McMoveFromBaronGeddonAction::Execute(Event event)
 {
-    const float radius = 25.0f;  // more than should be needed but bots keep trying to run back in
     if (Unit* boss = AI_VALUE2(Unit*, "find target", "baron geddon"))
     {
+        constexpr float radius = 25.0f; // more than should be needed but bots keep trying to run back in
         long distToTravel = radius - bot->GetDistance(boss);
         if (distToTravel > 0)
         {
             // float angle = bot->GetAngle(boss) + M_PI;
             // return Move(angle, distToTravel);
-            return MoveAway(boss, distToTravel); 
+            return MoveAway(boss, distToTravel);
         }
     }
     return false;
--- src/strategy/raids/moltencore/RaidMcTriggers.h
+++ src/strategy/raids/moltencore/RaidMcTriggers.h
@@ -5,6 +5,13 @@
 #include "Playerbots.h"
 #include "Trigger.h"
 
+enum SpellIdsMC
+{
+    // Baron Geddon
+    SPELL_BARON_GEDDON_INFERNO = 19695,
+    SPELL_BARON_GEDDON_LIVING_BOMB = 20475,
+};
+
 class McLivingBombDebuffTrigger : public Trigger
 {
 public:
